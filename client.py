c3e484de5c123e257655e561fd85ec401d70745c
Matheus Paz Esteves <matheuspazesteves@hotmail.com>
2023-05-17 21:14:51 -0300

    Update client.py

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ import pickle
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((HOST, PORT)) # connect to server (block until accepted)
 msg = ('add',['1','2'])
-s.send(pickle.dump(msg))  # send some data
+s.send(pickle.dumps(msg))  # send some data
 data = s.recv(1024)     # receive the response
 print (pickle.load(data))            # print the result
 s.close()               # close the connection
